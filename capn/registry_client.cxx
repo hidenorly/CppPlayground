8a5b5a9bf41e14c2e9d6bcb5cf2868e551dea9c6
hidenorly <twitte.harold@gmail.com>
2025-11-07 17:16:47 +0900

    Add helper template

--- capn/registry_client.cxx
+++ capn/registry_client.cxx
@@ -28,6 +28,62 @@
 
 #include "registry.hpp"
 
+template <typename T> class ClientBase
+{
+protected:
+  std::shared_ptr<capnp::EzRpcClient> mpClient;
+  std::shared_ptr<T> mpClientImpl;
+  std::string mSocketPath;
+  std::string mUnixsocketPath;
+  std::vector<uint32_t> mCallbackIds;
+
+public:
+  ClientBase(std::string socketPath):mSocketPath(socketPath){
+    mUnixsocketPath = "unix:"+socketPath;
+
+    mpClient = std::make_shared<capnp::EzRpcClient>(mUnixsocketPath);
+    if(mpClient){
+      mpClientImpl = std::make_shared<T>(mpClient->getMain<Registry>());
+    }
+  }
+
+  virtual ~ClientBase(){
+    bool isAvailable = mpClient && mpClientImpl;
+
+    if( isAvailable ){
+      for(auto& id : mCallbackIds){
+        unregisterCallback(id);
+      }
+      mCallbackIds.clear();
+    }
+  }
+
+  virtual uint32_t registerCallback(const std::string id, Callback::Server callback){
+    uint32_t resultId = -1;
+    bool isAvailable = mpClient && mpClientImpl;
+
+    if( isAvailable ){
+      auto regReq = mpClientImpl->registerCallbackRequest();
+      regReq.setCb(kj::mv(callback));
+      resultId = regReq.send().wait(mpClient->getWaitScope()).getId();
+      mCallbackIds.push_back(resultId);
+    }
+
+    return resultId;
+  }
+
+  virtual void unregisterCallback(const uint32_t id){
+    bool isAvailable = mpClient && mpClientImpl;
+    if( isAvailable ){
+      auto unregReq = mpClientImpl->unregisterCallbackRequest();
+      unregReq.setId(id);
+      unregReq.send().wait(mpClient->getWaitScope());
+    }
+    std::erase_if(mCallbackIds, [&](uint32_t theId){ return theId == id; });
+  }
+};
+
+
 typedef std::function<void(const std::string&, const std::string&)> NOTIFIER;
 
 class LambdaCallbackHandler final : public Callback::Server {
@@ -47,6 +103,8 @@ public:
 private:
   std::string mHandlerName;
 };
+
+
 
 
 class RegistryClient : public MyInterface
